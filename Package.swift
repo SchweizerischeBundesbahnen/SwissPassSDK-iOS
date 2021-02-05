1b668a9e69f1e128c86ceba745180a30f1b34552
jenkins <no-reply@sbb.ch>
2021-02-05 16:40:00 +0100

    Updated

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "SwissPassClient",
+        platforms: [
+            .iOS(.v11),
+        ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SwissPassClient",
+            targets: ["SwissPassClient"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .binaryTarget(name: "SwissPassClient",
+                      url: "https://github.com/SchweizerischeBundesbahnen/SwissPassSDK-iOS/releases/download/3.0.2/SwissPassClient_framework-3.0.2_r210205.1639.4.zip",
+                      checksum: "81ccfdbc26c0e13447ef3580ff8ed9e43fbf796904549b832abd26bb22bce541"),
+    ]
+)
