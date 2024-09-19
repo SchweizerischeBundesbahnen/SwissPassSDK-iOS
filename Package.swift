ef8242654929dd9a186b6fcfd176b0fee70a478a
Philippe Robert <philippe.robert@gmail.com>
2024-09-19 09:43:04 +0200

    Reverted Package.swift

--- Package.swift
+++ Package.swift
@@ -10,9 +10,6 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwissPassLogin",
-            targets: ["SwissPassLogin"]),
-        .library(
             name: "SwissPassClient",
             targets: ["SwissPassClient"])
     ],
@@ -21,11 +18,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "SwissPassLogin",
-                      url: "https://github.com/SchweizerischeBundesbahnen/SwissPassSDK-iOS/releases/download/4.3.2/SwissPassClient_framework-4.3.2_r240919.0923.12.zip",
-                      checksum: "83334d1b65abc62ba8ed474b1d3b5983675f83fb76ff7ac780d629d2312288e7"),
         .binaryTarget(name: "SwissPassClient",
-                      url: "${URL}",
-                      checksum: "${CHECKSUM}")
+                      url: "https://github.com/SchweizerischeBundesbahnen/SwissPassSDK-iOS/releases/download/4.3.1/SwissPassClient_framework-4.3.1_r240618.0844.10.zip",
+                      checksum: "5e8237db67ac03e9a08d214203a17d156d4cafd7d34cc2ef6937fd0e55794f2d"),
     ]
 )
