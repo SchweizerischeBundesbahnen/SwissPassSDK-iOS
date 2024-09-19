32f1146cbd68a35db473f125c5c68d126a3bf7a3
ci-mobile-jenkins <ci-mobile@sbb.ch>
2024-09-19 09:21:56 +0200

    Updated

--- Package.swift
+++ Package.swift
@@ -10,6 +10,9 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
+            name: "SwissPassLogin",
+            targets: ["SwissPassLogin"]),
+        .library(
             name: "SwissPassClient",
             targets: ["SwissPassClient"])
     ],
@@ -18,8 +21,11 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        .binaryTarget(name: "SwissPassLogin",
+                      url: "https://github.com/SchweizerischeBundesbahnen/SwissPassSDK-iOS/releases/download/4.3.2/SwissPassLogin_framework-4.3.2_r240919.0921.12.zip",
+                      checksum: "202ec20b785b0ac20610c6825cbfcef9a0efbbf988cdd428b8f2a89e38e93fa4"),
         .binaryTarget(name: "SwissPassClient",
-                      url: "https://github.com/SchweizerischeBundesbahnen/SwissPassSDK-iOS/releases/download/4.3.1/SwissPassClient_framework-4.3.1_r240618.0844.10.zip",
-                      checksum: "5e8237db67ac03e9a08d214203a17d156d4cafd7d34cc2ef6937fd0e55794f2d"),
+                      url: "${URL}",
+                      checksum: "${CHECKSUM}")
     ]
 )
