a5827ad7abcbc1ad7972958f61298538966630a7
ci-mobile-jenkins <ci-mobile@sbb.ch>
2024-12-20 18:15:58 +0100

    Updated

--- Package.swift
+++ Package.swift
@@ -17,8 +17,9 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
+    targets: [
         .binaryTarget(name: "SwissPassClient",
-                      url: "https://github.com/SchweizerischeBundesbahnen/SwissPassSDK-iOS/releases/download/4.3.2/SwissPassClient_framework-4.3.2_r241219.1429.13.zip",
-                      checksum: "40b235551a9efb50af8a7d93c421048c756f41699535763f73bd4126fa92ee3f")
+                      url: "https://github.com/SchweizerischeBundesbahnen/SwissPassSDK-iOS/releases/download/4.3.2/SwissPassClient_framework-4.3.2_r241220.1815.14.zip",
+                      checksum: "f64cce72b71bae7deba27a750f6c4214a2a190289bde15a2c03126b61876b389")
     ]
 )
